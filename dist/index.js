6b02f03a81f9a399f62f459381649672f14b9736
pransandip <[pransandip@gmail.com]>
2023-03-11 19:08:01 +0530

    open issue: to start server with ESNext config

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+const app = express();
+const PORT = 4000;
+let count = 0;
+app.get("/", (req, res) => {
+    count += 1;
+    console.log(`IP: ${req.ip} hits server: ${count} time.`);
+    res.status(200).json({ msg: "healthy" });
+});
+app.listen(PORT, () => {
+    console.log(`TS: server is running on port 🚀 ${PORT}.`);
+});
+//# sourceMappingURL=index.js.map
